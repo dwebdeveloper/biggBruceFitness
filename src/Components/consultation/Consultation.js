8138be15d8a0db72c3168ffe10ad489a64002e6e
James Farfan <jamesrojas710@gmail.com>
2020-11-16 08:29:08 -0500

    moved consultation container into its own file, creating a folder as well

---
+++ src/Components/consultation/Consultation.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import RegistrationForm from '../../Components/registrationform/RegistrationForm';
+
+class Consultation extends Component {
+
+
+    render() {
+        <div className='Consultation'>
+
+            <p className='ConsultationContent'>
+                <h2 className='ConsultationTitle'>
+                    Free Consultation
+                </h2>
+                    Fitness goals - nutritional goals - life goals. Lets talk and design the right program for you.
+                <a href={RegistrationForm} className='LetsBegin'>LETS BEGIN</a>
+
+            </p>
+        </div>
+    }
+}
+
+export default Consultation;
