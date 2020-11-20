12de6ad44781767df45d2f825836a5b81f4989ef
James Farfan <jamesrojas710@gmail.com>
2020-11-19 22:02:06 -0500

    responsive desgin for about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../Pages/About.css'
 import BBF from '../assets/images/BBF1.jpg';
+/*import Rashad from '../assets/images/Rashad.jpg';*/
 
 
 class About extends Component {
@@ -9,10 +10,9 @@ class About extends Component {
             <div className='AboutContainer'>
                 <img className='AboutImg' src={BBF} alt='Rashad' />
 
-
                 <p className='AboutMe'>
                     <h1>RASHAD AKA BIGG BRUCE</h1>
-                    I am NASM certified personal trainer.
+                    I am NASM certified personal trainer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ab laboriosam deserunt? Quia pariatur placeat atque nesciunt soluta totam, aliquid voluptatibus repudiandae, ab at vitae! Ex labore aliquid impedit enim!
                 </p>
             </div>
         )
